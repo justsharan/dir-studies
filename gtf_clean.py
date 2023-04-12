f3ff4d92ff090c7cc72928ca1908af147dab3d6c
Sharan S <sharansr@pm.me>
2023-04-12 12:15:42 -0700

    Use sys.stdout.write to prevent unnecessary newline

--- gtf_clean.py
+++ gtf_clean.py
@@ -6,6 +6,6 @@ with open(filename, 'r') as file:
     for line in file:
         split = line.split('\t')
         if len(split) != 9:
-            print(line)
+            sys.stdout.write(line)
         elif split[6] != '?':
-            print(line)
+            sys.stdout.write(line)
