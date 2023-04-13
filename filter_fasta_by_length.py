19dced906cee85103e85752a2c99bab45714fee8
Sharan S <sharansr@pm.me>
2023-04-13 14:28:02 -0700

    Added script that filters fasta file by specified length

---
+++ filter_fasta_by_length.py
@@ -0,0 +1,18 @@
+from argparse import ArgumentParser
+from Bio import SeqIO
+
+parser = ArgumentParser()
+parser.add_argument('-l', '--len', help='Maximum length to filter by (inclusive)', type=int)
+parser.add_argument('-o', '--output', help='Output file')
+parser.add_argument('filename')
+
+args = parser.parse_args()
+
+filtered_records = [
+    fasta_record
+    for fasta_record in SeqIO.parse(args.filename, 'fasta')
+    if len(fasta_record.seq) <= args.len
+]
+
+output_file = open(args.output, 'w')
+SeqIO.write(filtered_records, output_file, 'fasta')
