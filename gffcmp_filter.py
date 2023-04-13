7572bd4e443605b50142da952029603b41190dd7
Sharan S <sharansr@pm.me>
2023-04-12 21:50:59 -0700

    Made file a positional argument

--- gffcmp_filter.py
+++ gffcmp_filter.py
@@ -1,15 +1,15 @@
 from argparse import ArgumentParser
 import pandas as pd
-import sys
 
 parser = ArgumentParser()
 parser.add_argument('-c', '--code', help='Class code')
 parser.add_argument('-l', '--len', help='Maximum transcript length (inclusive)')
 parser.add_argument('-o', '--output', help='Output file')
+parser.add_argument('filename')
 
 args = parser.parse_args()
 
-gffcmp_file = pd.read_csv(sys.argv[1])
+gffcmp_file = pd.read_csv(args.filename)
 filtered = gffcmp_file[(gffcmp_file['class_code'] == args.code) & (gffcmp_file['len'] <= args.len)]
 
 filtered.to_csv(args.output)
