fe5eda0006417d399d45c97067bae618266d7bc1
Sharan S <sharansr@pm.me>
2023-04-12 21:45:33 -0700

    Added length argument to script

--- gffcmp_filter.py
+++ gffcmp_filter.py
@@ -4,11 +4,12 @@ import sys
 
 parser = ArgumentParser()
 parser.add_argument('-c', '--code', help='Class code')
+parser.add_argument('-l', '--len', help='Maximum transcript length (inclusive)')
 parser.add_argument('-o', '--output', help='Output file')
 
 args = parser.parse_args()
 
 gffcmp_file = pd.read_csv(sys.argv[1])
-filtered = gffcmp_file[gffcmp_file['class_code'] == args.code]
+filtered = gffcmp_file[(gffcmp_file['class_code'] == args.code) & (gffcmp_file['len'] <= args.len)]
 
 filtered.to_csv(args.output)
